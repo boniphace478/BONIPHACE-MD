dc72712de238e50a088e64a49b6f19b8e1568e2f
Boniphace Tech <anthonyboniphace478@gmail.com>
2024-08-03 16:32:32 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -7,7 +7,7 @@ const { default: axios } = require('axios');
 zokou({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🤞', 
+    reaction: '🚀', 
     fromMe: 'true', 
 
        
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Lucky 🍀 V5 respond speed is*\n ```' +1045+ '``` *m/s*') 
+    return repondre('*BONIPHACE -MD respond speed is*\n ```' +6890+ '``` *m/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
