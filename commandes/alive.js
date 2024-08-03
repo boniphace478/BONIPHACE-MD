3cfdeec3a7bcffddff92863b1db4f1ee3e6ef765
Boniphace Tech <anthonyboniphace478@gmail.com>
2024-08-03 15:06:03 +0300

    Update alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -43,7 +43,7 @@ const date = moment().format('DD/MM/YYYY');
  ${message}
  
  
- *LUCKY🍀-MD-V5-WABOT*`
+ *BONIPHACE🍀-MD-WABOT*`
 
  if (lien.match(/\.(mp4|gif)$/i)) {
     try {
@@ -88,7 +88,7 @@ else {
     
 await addOrUpdateDataInAlive(texte , tlien)
 
-repondre(' Hello👋 , *LUCKY_MD BOT* is alive just like you gee😜. ')
+repondre(' Hello👋 , I'm *BONIPHACE-MD* created by boniphace tech😜. ')
 
 }
     });
