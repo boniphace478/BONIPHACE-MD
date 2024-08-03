e76c5bf4f7d47e3864d7bddb542ff5a1de9e7850
Boniphace Tech <anthonyboniphace478@gmail.com>
2024-08-03 15:50:35 +0300

    Update conversion.js

--- commandes/conversion.js
+++ commandes/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Hacking-Md" ,
+      pack:"Boniphace-Md" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
@@ -94,7 +94,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Hacking-Md", // pack stick
+      pack:"Boniphace-Md", // pack stick
       author:  nomAuteurMessage, // name of the author of the stick
       type:
         arg.includes("-r") || arg.includes("-c")
