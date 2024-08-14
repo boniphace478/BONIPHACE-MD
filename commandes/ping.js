bd17b42a77f2eb88d28b964929d313cef4fbe4ac
Boniphace Tech <176963808+boniphace478@users.noreply.github.com>
2024-08-14 23:04:56 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*BONIPHACE-MD 🚀 respond speed is*\n ```' +6890+ '``` *m/s*') 
+    return repondre('*𝔹𝕆ℕ𝕀ℙℍ𝔸ℂ𝔼~𝕄𝔻 💫 respond speed is*\n ```' +7890+ '``` *m/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
