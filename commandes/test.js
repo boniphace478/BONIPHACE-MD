85407747f996bccc75f42ae1aeee48df8986fa22
BoniphaceTech <anthonyboniphace478@gmail.com>
2024-08-18 11:22:27 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -6,7 +6,7 @@ zokou({ nomCom: "test", reaction: "📎", nomFichier: __filename }, async (dest,
     let z = 'Hello my name is  *𝔹𝕆ℕ𝕀ℙℍ𝔸ℂ𝔼 𝕄𝔻* \n\n ' + "i'm a whatsapp bot multi-device created ";
     let d = ' by *𝔹𝕆ℕ𝕀ℙℍ𝔸ℂ𝔼 𝕋𝔼ℂℍ*';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/06a4537021d18bca5753c.mp4';
+    var img = 'https://telegra.ph/file/1c7db35f3445430d1bbee.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -22,6 +22,6 @@ async function  execute  (origineMessage,zok) {
    let z ='Salut je m\'appelle *BONIPHACE* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *Boniphace*'
       let varmess=z+d
-      var img='https://telegra.ph/file/06a4537021d18bca5753c.mp4'
+      var img='https://telegra.ph/file/1c7db35f3445430d1bbee.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
