9dda05134f5a920d8f122fc6205a702e9266e030
Boniphace Tech <anthonyboniphace478@gmail.com>
2024-08-28 11:15:06 +0300

    Update and rename ping to ping.js

---
+++ commandes/ping.js
@@ -0,0 +1,23 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+zokou({ nomCom: 'ping',
+    desc: 'To check ping',
+    Categorie: 'General',
+    reaction: '🚀', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*𝗕𝗢𝗡𝗜𝗣𝗛𝗔𝗖𝗘~𝗠𝗗 💥        respond speed is*\n ```' +1000000990+ '``` *m/s*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
+  }
+)
+
